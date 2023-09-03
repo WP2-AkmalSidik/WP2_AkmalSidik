1194d13c01d9b014702a0cf58eb7ddcdc360d983
WP2-AkmalSidik <akmalsidik18@gmail.com>
2023-09-03 17:25:03 +0700

    Saya Telah mengerjakan Contoh1, Yaitu tentang Menampilkan biodata

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,20 @@
+<?php
+
+class Contoh1 extends CI_Controller{
+    public function index()
+    {
+        echo "<h1>Bio Data</h1>";
+        echo"Nama: Akmal Sidik <br>
+        Tempat dan Tanggal Lahir: Tasikmalaya, 18-09-2003 <br>
+        Alamat: Jln. Ciwaas Depok 1 Kec.Tamansari <br>
+        Pendidikan Terakhir: SMA <br>
+        Pekerjaan: Pelajar <br>
+        Hobi: Traveling <br>
+        Deskripsi Singkat: <br>
+        
+        Saya sekarang sedang kuliah di univ BSI Tasikmalaya <br>dan baru menjakani Smester 3 di kelas yang berbeda";
+       
+    }
+}
+
+?>
